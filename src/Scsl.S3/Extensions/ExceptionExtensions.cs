9165c72a88a3f1fbecaf81dcc225492e9fc37cf5
Shane Saunders <asparatu@gmail.com>
2024-12-04 16:46:34 +0800

    Add Xml summary

--- src/Scsl.S3/Extensions/ExceptionExtensions.cs
+++ src/Scsl.S3/Extensions/ExceptionExtensions.cs
@@ -8,6 +8,13 @@ namespace Scsl.S3.Extensions;
 
 internal static class ExceptionExtensions
 {
+    /// <summary>
+    /// Converts an <see cref="AmazonS3Exception"/> into a list of <see cref="S3ClientError"/> objects,
+    /// capturing the error code and message details.
+    /// </summary>
+    /// <param name="ex">The <see cref="AmazonS3Exception"/> to process.</param>
+    /// <returns>A list of <see cref="S3ClientError"/> objects with error details.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if the exception parameter is null.</exception>
     public static List<S3ClientError> ToS3ClientErrors(this AmazonS3Exception ex)
     {
         List<S3ClientError> errors = [];
@@ -27,6 +34,12 @@ internal static class ExceptionExtensions
         return errors;
     }
     
+    /// <summary>
+    /// Converts an exception to a list of <see cref="S3ClientError"/> objects for error reporting.
+    /// </summary>
+    /// <param name="ex">The exception to convert.</param>
+    /// <returns>A list of <see cref="S3ClientError"/> objects containing error details.</returns>
+    /// <exception cref="NullReferenceException">Thrown if the inner exception is null when accessing its message.</exception>
     public static List<S3ClientError> ToS3ClientErrors(this Exception ex)
     {
         List<S3ClientError> errors = [];
@@ -49,6 +62,11 @@ internal static class ExceptionExtensions
         return errors;
     }
     
+    /// <summary>
+    /// Generates a list containing a single S3 client error indicating an internal server error.
+    /// </summary>
+    /// <returns>A list of <see cref="S3ClientError"/> objects with a predefined error.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if the list initialization fails.</exception>
     public static List<S3ClientError> FailErrors()
     {
         List<S3ClientError> errors = [];
@@ -61,6 +79,13 @@ internal static class ExceptionExtensions
         return errors;
     }
     
+    /// <summary>
+    /// Creates a list containing a single S3 client error indicating a file not found.
+    /// </summary>
+    /// <returns>A list of <see cref="S3ClientError"/> with an error code and description for "File Not Found".</returns>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown if the <see cref="S3ClientError"/> object initialization fails.
+    /// </exception>
     public static List<S3ClientError> FileNotFoundError()
     {
         List<S3ClientError> errors = [];
